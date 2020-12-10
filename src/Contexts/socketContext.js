ba9e4d746f2d926ad1daa35884b06f69b64fa1cb
U Ahmad <UmadAhmad1928@gmail.com>
2020-12-10 14:38:03 +0500

    Changed Links to deployment Links

--- src/Contexts/socketContext.js
+++ src/Contexts/socketContext.js
@@ -6,8 +6,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = (props) => {
    // socket = socketIo.connect('localhost:5000', {
-   // socket = socketIo.connect('https://mern-chat-project.herokuapp.com', {
-   socket = socketIo.connect('localhost:5000', {
+   socket = socketIo.connect('https://mern-chat-project.herokuapp.com', {
       transports: ['websocket'],
    });
    socket.on('connect', function () {
