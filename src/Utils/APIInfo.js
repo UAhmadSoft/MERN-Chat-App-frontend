40ce8ba8452faaf2998469c4ba93d231bf646cb6
U Ahmad <UmadAhmad1928@gmail.com>
2020-12-10 15:14:59 +0500

    fixed small bug  on api url

--- src/Utils/APIInfo.js
+++ src/Utils/APIInfo.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://mern-chat-project.herokuapp.com/';
+export const API_URL = 'https://mern-chat-project.herokuapp.com';
 // export const API_URL = 'http://localhost:5000';
